9848aacc00301e32a80b97aa9e247ab3c3104721
TeannaB2023 <teanna.barrett@bison.howard.edu>
2021-01-21 20:10:44 -0600

    Testing different password

--- LSP_Assignments/src/org/howard/edu/lsp/assignment1/HelloWorld.java
+++ LSP_Assignments/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// Print out 'Hello World' in the console 
-		// Name: Teanna Barrett
+		// Name: Teanna Barrett 
 		System.out.println("Hello World!!");
 	}
 
