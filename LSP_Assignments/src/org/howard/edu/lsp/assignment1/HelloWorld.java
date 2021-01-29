ad9ab01949c9a328c3f235f753da9fa4312d8a89
TeannaB2023 <teanna.barrett@bison.howard.edu>
2021-01-28 19:16:11 -0600

    updated commenting to style

--- LSP_Assignments/src/org/howard/edu/lsp/assignment1/HelloWorld.java
+++ LSP_Assignments/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -1,10 +1,14 @@
 package org.howard.edu.lsp.assignment1;
 
+/**
+ * Prints out 'Hello World' in the console  
+ * @author barre 
+ * aka Teanna Barrett
+ */
+
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// Print out 'Hello World' in the console 
-		// Name: Teanna Barrett 
 		System.out.println("Hello World!!");
 	}
 
