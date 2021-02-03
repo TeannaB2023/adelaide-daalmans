6fefdf9c9f10afede48b87e0aafc2d334f546af4
TeannaB2023 <teanna.barrett@bison.howard.edu>
2021-02-03 00:06:09 -0600

    Initial set up for assignment 2

---
+++ LSP_Assignments/src/org/howard/edu/lsp/assignment2/combination/Combination.java
@@ -0,0 +1,20 @@
+package org.howard.edu.lsp.assignment2.combination;
+
+import java.util.ArrayList;
+import java.util.Set;
+
+public class Combination {
+	
+	ArrayList elements;
+	int target;
+	Set combination;
+	Set combinations;
+	
+	void Combination (ArrayList elements, int target) {
+		this.elements = elements;
+		this.target = target;
+	}
+	
+	void calculateCombination (){}
+
+}
---
+++ LSP_Assignments/src/org/howard/edu/lsp/assignment2/test/Driver.java
@@ -0,0 +1,16 @@
+package org.howard.edu.lsp.assignment2.test;
+
+/**
+ * The adding program will be run and tested here  
+ * @author barre 
+ * aka Teanna Barrett
+ */
+
+public class Driver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
