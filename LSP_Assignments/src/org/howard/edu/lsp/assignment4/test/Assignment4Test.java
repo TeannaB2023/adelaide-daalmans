5583283a744295e56ec4da54b0f20ea969bcac08
TeannaB2023 <teanna.barrett@bison.howard.edu>
2021-03-31 04:22:47 -0500

    added JUnit to build path

---
+++ LSP_Assignments/src/org/howard/edu/lsp/assignment4/test/Assignment4Test.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package org.howard.edu.lsp.assignment4.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author barre
+ *
+ */
+class Assignment4Test {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
